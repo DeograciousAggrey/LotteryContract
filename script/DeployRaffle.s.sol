f971cc5a42434a2ebad6ac14db11dcaa5ad97bef
DeograciousAggrey <deograciousaggrey@gmail.com>
2023-08-28 15:32:18 +0300

    Updated Deploy scripts

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -3,7 +3,30 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployRaffle is Script {
-    function run() external returns (Raffle) {}
+    function run() external returns (Raffle) {
+        HelperConfig helperConfig = new HelperConfig();
+        (
+            uint256 _entranceFee,
+            uint256 _interval,
+            address _vrfCoordinator,
+            bytes32 _gasLane,
+            uint64 _subscriptionId,
+            uint32 _callbackGasLimit
+        ) = helperConfig.activeNetworkConfig();
+
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(
+            _entranceFee,
+            _interval,
+            _vrfCoordinator,
+            _gasLane,
+            _subscriptionId,
+            _callbackGasLimit
+        );
+        vm.stopBroadcast();
+        return raffle;
+    }
 }
